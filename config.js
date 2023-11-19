ca0265a06761fc59bafbf51609277529ba217b85
cupcake176 <151464065+cupcake176@users.noreply.github.com>
2023-11-19 10:33:49 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "If u're reading this, u're cute",
+    introTitle: 'Akira',
+    introDesc: `I love ur voice, I love ur smile, I love ur personalities, everything about u <3`,
+    btnIntro: 'thank u :3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy',
+    btnYes: 'Yes ofc :3',
+    btnNo: 'not even close :<',
+    question: 'There are more than 7 people in the world, why me ?',
+    btnReply: 'send u my love',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'wanna date? 😘😘',
+    messDesc: 'i mean right now :>',
+    btnAccept: 'forget about it just get married <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
